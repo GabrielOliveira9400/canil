0f8a528b7e4917e576c576fe3f8a5e5cd79d25be
Gabriel Oliveira <gabriel.alves9400@hotmail.com>
2021-10-02 00:13:19 -0300

    Controllers, rotas e views canil

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,14 @@
+import {Request,Response} from "express";
+
+export const home = (req:Request,res:Request)=>{
+    //res.render('pages/page');
+}
+export const gatos = (req:Request,res:Request)=>{
+    //res.render('pages/page');
+}
+export const cachorros = (req:Request,res:Request)=>{
+    //res.render('pages/page');
+}
+export const peixes = (req:Request,res:Request)=>{
+    //res.render('pages/page');
+}
---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import mustacheExpress from "mustache-express";
+import mainRoutes from './routes/index';
+dotenv.config();
+const server = express();
+server.set('view engine', 'mustache');
+server.set('views',path.join(__dirname, 'views'));
+server.engine('mustache',mustacheExpress());
+server.use(express.static(path.join(__dirname,'../public')));
+
+
+//rotas
+server.use(mainRoutes);
+
+//Rota 404
+server.use((req, res )=>{
+    res.status(404).send('Pagina Não Encontrada');
+});
+
+
+server.listen(3000);
